a21f5d6c8084c845f7d8f103b960b6685574cdba
brianchandraab <brian.sevenfold@gmail.com>
2019-07-24 23:19:43 +0700

    perbaikan kejadian

--- web/ajax_pelapor_hapus.php
+++ web/ajax_pelapor_hapus.php
@@ -3,6 +3,8 @@
 include '../functions/kumpulan_fungsi.php';
 $id_masyarakat_pelapor = isset($_REQUEST['id_mp']) ? $_REQUEST['id_mp'] : 0;
 $kon = koneksi_db();
+//echo $id_masyarakat_pelapor;
+//exit();
 
 $query = mysqli_query($kon, "DELETE FROM masyarakat_pelapor WHERE id_masyarakat_pelapor='$id_masyarakat_pelapor'");
 
